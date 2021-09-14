5fcf8f0dff559a99c9f9742d07a6ba891e0a5272
Julian krochmal <jkrochmal.ext@andromedalatam.com>
2021-09-13 22:13:42 -0300

    deleting unnecesary lines

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,12 +1,10 @@
 type ErrorProps = {
     text: string
 }
-const Error = ({ text }: ErrorProps) => {
-    return (
-        <div>
-            <p className='red darken-4 error'> {text}</p>
-        </div>
-    )
-}
+const Error = ({ text }: ErrorProps) => (
+    <div>
+        <p className='red darken-4 error'> {text}</p>
+    </div>
+)
 
 export default Error
